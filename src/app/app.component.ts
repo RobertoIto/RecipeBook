8e38674237839c0cdd26212eb25bb9330d8a1973
Rito <rito@my.centennialcollege.ca>
2019-04-24 20:09:36 -0400

    81 Navigation with binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RecipeBook';
+  loadedFeature = 'recipe';
+
+  onNavigate(opt: string) {
+    this.loadedFeature = opt;
+  }
 }

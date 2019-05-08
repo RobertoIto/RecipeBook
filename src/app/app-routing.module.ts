d6249c33f74b4695dda8782fa958ca8c8b0e46fd
Rito <rito@my.centennialcollege.ca>
2019-05-07 22:48:22 -0400

    299 - Preloading Lazy Loaded routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomeComponent } from './core/home/home.component';
 
-// The path recipes is loaded lazyly, we pass the path # and the
+// The path recipes is loaded lazily, we pass the path # and the
 // class name. In the recipes-routing.module the Recipes path must
 // be changed to empty.
 const appRoutes: Routes = [
@@ -16,8 +16,13 @@ const appRoutes: Routes = [
   // { path: '**', redirectTo: '/recipes' }
 ];
 
+// Above, we have the recipes that uses the lazy loading. In the
+// RouterModule.forRoot, we add a new parameter that allows
+// Angular to preload the lazy loading modules.
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(
+              appRoutes,
+              {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 
